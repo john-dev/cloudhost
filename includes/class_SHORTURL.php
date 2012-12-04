d4ff76466cfef8659abac4d516d7b4e149078eb9
john-dev <joh.dev@gmail.com>
2012-12-04 16:47:34 +0100

    added google stats

--- includes/class_SHORTURL.php
+++ includes/class_SHORTURL.php
@@ -11,9 +11,18 @@ class handleSHORTURL {
         return $returnArray->id;
     }
     
-    private function postData($url, $data) {
+    public function getHITS_SIMPLE($shortUrl) {
+        $dataJson=array("");
+        $returnJson = $this->postData("https://www.googleapis.com/urlshortener/v1/url?shortUrl=".$shortUrl."&projection=FULL", $dataJson, 0);
+ 
+        $returnArray = json_decode($returnJson,true);
+
+        return $returnArray['analytics']['allTime']['shortUrlClicks'];
+    }
+       
+    private function postData($url, $data, $post=1) {
         $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POST, $post);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
