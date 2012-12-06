229e594b8a56b7c6b0b2ecd774e0b8f9fd5510e7
john dev <joh.dev@gmail.com>
2012-12-06 15:37:16 +0100

    Update includes/add_user.php

--- includes/add_user.php
+++ includes/add_user.php
@@ -13,7 +13,7 @@ if(isset($argv[2])) {
 }
 include('config.php');
 if(!$user) {
-   die("wrong usage: add_user.php user pass\n"); 
+   die("wrong usage: add_user.php user [pass]\n"); 
 }
 if(!$pass) {
     //expect cloudapp auth use
@@ -24,6 +24,9 @@ if(!$pass) {
     $stmt->execute();
 } else {
     //expect cloudhost as auth server
+    if(substr($pass,0,1=='"') {
+        $pass=substr($pass,1,-1);
+    }
     $db=new mysqli( DB_HOST , DB_USER , DB_PASS , DB_NAME);
     $sql="replace into user set email=?,password=?";
     $stmt=$db->prepare($sql);
